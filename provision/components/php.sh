50133691ee0c783f73831df1dec9cfe151dc59d8
Johnny Brandom <johnnyBrandom@ojoti.com>
2024-03-02 12:21:05 -0500

    Change install from specific php version to default available in repo. Add support to determine installed php version before modifying php.ini.

--- provision/components/php.sh
+++ provision/components/php.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-sudo apt-get install software-properties-common
-sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
+sudo apt-get install -y software-properties-common
+#sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
+sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/apache2
 sudo apt-get update
-sudo apt-get install -y php7.4 php7.4-bcmath php7.4-bz2 php7.4-cli php7.4-curl php7.4-intl php7.4-json php7.4-mbstring php7.4-opcache php7.4-soap php7.4-xml php7.4-xsl php7.4-zip libapache2-mod-php7.4 php7.4-mysql php7.4-gd composer
+sudo apt-get install -y php php-bcmath php-bz2 php-cli php-curl php-intl php-json php-mbstring php-opcache php-soap php-xml php-xsl php-zip libapache2-mod-php php-mysql php-gd
 
-sed -i 's/max_execution_time = .*/max_execution_time = 60/' /etc/php/7.4/apache2/php.ini
-sed -i 's/post_max_size = .*/post_max_size = 64M/' /etc/php/7.4/apache2/php.ini
-sed -i 's/upload_max_filesize = .*/upload_max_filesize = 1G/' /etc/php/7.4/apache2/php.ini
-sed -i 's/memory_limit = .*/memory_limit = 512M/' /etc/php/7.4/apache2/php.ini
+# Get the php version that was installed by querying the /etc/php subdir
+# Note this command expects a single subdirectory
+
+IFS=$'\n' read -r -d '' -a PHPV <<< "$(ls /etc/php)"
+
+# Modify parameters in php.ini
+
+sed -i 's/max_execution_time = .*/max_execution_time = 60/' /etc/php/$PHPV/apache2/php.ini
+sed -i 's/post_max_size = .*/post_max_size = 64M/' /etc/php/$PHPV/apache2/php.ini
+sed -i 's/upload_max_filesize = .*/upload_max_filesize = 1G/' /etc/php/$PHPV/apache2/php.ini
+sed -i 's/memory_limit = .*/memory_limit = 512M/' /etc/php/$PHPV/apache2/php.ini
 
 # Enable gd.ini
+
 sudo phpenmod gd
 sudo service apache2 restart
