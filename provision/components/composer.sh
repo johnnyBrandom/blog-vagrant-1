fc3379255cdce31e22a8829f98c0008e83e17c39
Johnny Brandom <johnnyBrandom@ojoti.com>
2024-03-04 00:34:04 -0500

    Introduced provision script for installing composer under more recent Ubuntu releases.

---
+++ provision/components/composer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+HASH=`curl -sS https://composer.github.io/installer.sig`
+echo $HASH
+php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+
+if [ -f /tmp/composer-setup.php ]; then
+  sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+fi
